ac13e13cbd2d46b8fc4a48287dbd17aee9cead13
Sangameeee <137791865+Sangameeee@users.noreply.github.com>
2025-05-20 09:33:07 +0545

    changes made

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from google import genai
 import markdown
 import os
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.secret_key = os.getenv('KEY')
 load_dotenv()
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI') 
 db = SQLAlchemy(app)
@@ -29,23 +30,31 @@ def gemini_call(question):
     answer = markdown.markdown(response.text)
     return answer
 
+def add_history(question, answer):
+    session['history'].append({'question': question, 'answer': answer})
+    session.modified = True
+    
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    if 'history' not in session:
+        session['history'] = []
     if request.method == 'POST':
         question = request.form.get('question')
         cached = CacheAnswer.query.get(question)
         if cached:
             answer = cached.answer
             print('took cached value')
-            return f"<p>{answer}</p>"
+            add_history(question, answer)
+            return render_template('index.html', history = session['history'])
         else:
             answer = gemini_call(question)
             new_entry = CacheAnswer(question = question, answer = answer)
             db.session.add(new_entry)
             db.session.commit()
             print("saved to cached and showed it")
-            return f"<p>{answer}</p>"
+            add_history(question, answer)
+            return render_template('index.html', history = session['history'])
     return render_template('index.html')
 
 
